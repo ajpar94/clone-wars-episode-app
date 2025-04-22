b63b23b870eb38e5549d6cb82d02af684b08de8a
ajpar94 <parikh_ajit@hotmail.com>
2025-04-23 01:45:53 +0200

    :sparkles: adds fan-edits

--- clonewars.py
+++ clonewars.py
@@ -30,12 +30,41 @@ st.markdown('''
 <p align="center" style="color: #FFC62D;">
 This is my personal <i>Star Wars: The Clone Wars</i> episode order that skips over weaker or less relevant episodes and includes additional material from story reels and comics.  
 Episodes are grouped into story arcs and don't necessarily follow the chronological order, aiming to bring out the best of the series and make the story feel more cohesive and impactful.
-This list also includes links to episode guides and streaming options on Disney+ or YouTube.</p>
+This list also includes links to episode guides, fan-edits, and streaming options on Disney+ or YouTube.</p>
 ''', unsafe_allow_html=True)
 
-arc = ""
+arc = "Jedi Missions"
 
-for i, row in cw.iterrows():
+cw_header(arc)
+tabA, tabB = st.tabs(["Original", "Fan-Edit"])
+
+with tabA:
+    with st.expander("The Clone Wars Movie"):
+        col1, col2 = st.columns([10,18])
+        with col1:
+            img = "images/pilot.png"
+            st.image(img)
+        row = cw.iloc[0]
+        with col2:
+            if (q := row['quote']) != ".":
+                quote(q.upper())
+            st.write(row['desc'])
+            st.markdown(f"[Episode Guide]({row['url']}) | [Stream]({row['stream']})")
+
+with tabB:
+    with st.expander("The Clone Wars (S1E0: Battle of Christophsis)"):
+        col1, col2 = st.columns([10,18])
+        with col1:
+            img = "images/artwork.png"
+            st.image(img)
+        row = cw.iloc[0]
+        with col2:
+            if (q := row['quote']) != ".":
+                quote(q.upper())
+            st.write("The Clone Wars movie stripped down to a ~20 minute-long story that serves perfectly as the pilot episode. \n\nAnakin and Obi-Wan continue to fight on the frontlines in the Battle of Christophsis when they are surprised by a new visitor sent by the Jedi Council -- Padawan Ahsoka Tano.")
+            st.markdown(f"[Episode Guide]({row['url']}) | [Fan-Edit](https://originaltrilogy.com/topic/The-Clone-Wars-S1E0-Battle-of-Christophsis-Released/id/134671)")
+
+for i, row in cw[1:-4].iterrows():
     if row["arc_name"] != arc:
         if row['arc_name'].endswith("*"):
             cw_header_faded(f"{row['arc_name'][:-1]}")
@@ -47,7 +76,8 @@ for i, row in cw.iterrows():
         prefix = f"{row['season']}_{str.zfill(str(row['episode']), 2)} | "
     else:
         prefix = ""
-    with st.expander(prefix + row['title']):
+    expander_title = prefix + row['title']
+    with st.expander(expander_title):
         col1, col2 = st.columns([10,18])
         with col1:
             img = f"images/{row['title'].lower().replace(' ', '_')}.png"
@@ -57,3 +87,35 @@ for i, row in cw.iterrows():
                 quote(q.upper())
             st.write(row['desc'])
             st.markdown(f"[Episode Guide]({row['url']}) | [Stream]({row['stream']})")
+
+
+cw_header("Siege of Mandalore")
+tab1, tab2 = st.tabs(["Original", "Fan-Edit"])
+
+with tab2:
+    with st.expander("The Clone Wars (Revenge of the Sith -  Extended Edition)"):
+        col1, col2 = st.columns([10,18])
+        with col1:
+            img = f"images/artwork2.png"
+            st.image(img)
+        with col2:
+            quote(("It is in the darkest moments that the light within must shine the brightest").upper())
+            st.write("A combination of the final arc (S7\:E9-12) and Episode III - Revenge of the Sith, resulting in a 3.5h movie that concludes The Clone Wars.")
+            st.markdown(f"[Fan-Edit](https://originaltrilogy.com/topic/The-Clone-Wars-Revenge-of-the-Sith-Extended-Edition-Released/id/124945)")
+with tab1:
+    for i, row in cw.tail(4).iterrows():
+        if row['season'] != ".":
+            prefix = f"{row['season']}_{str.zfill(str(row['episode']), 2)} | "
+        else:
+            prefix = ""
+        expander_title = prefix + row['title']
+        with st.expander(expander_title):
+            col1, col2 = st.columns([10,18])
+            with col1:
+                img = f"images/{row['title'].lower().replace(' ', '_')}.png"
+                st.image(img)
+            with col2:
+                if (q := row['quote']) != ".":
+                    quote(q.upper())
+                st.write(row['desc'])
+                st.markdown(f"[Episode Guide]({row['url']}) | [Stream]({row['stream']})")
